f0e8fc9ba2f463fc522b7f1dbdc4669fef3bbe1f
Mahdi Naseri <mahdi.k.naseri@gmail.com>
2022-04-15 05:57:27 +0300

    Initial commit. Review NoteBook for more information.

---
+++ Foodies/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function() {
+    Route::get('/', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('index');
+    Route::resource('/categories', \App\Http\Controllers\Admin\CategoryController::class);
+    Route::resource('/menus', \App\Http\Controllers\Admin\MenuController::class);
+    Route::resource('/tables', \App\Http\Controllers\Admin\TableController::class);
+    Route::resource('/reservations', \App\Http\Controllers\Admin\ReservationController::class);
+});
+
+require __DIR__.'/auth.php';
