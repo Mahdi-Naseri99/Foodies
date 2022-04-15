c13b64d6eed842381103dc61242cd89200491bfa
Mahdi Naseri <mahdi.k.naseri@gmail.com>
2022-04-15 09:25:53 +0300

    Fifth Commit. Review NoteBook.

--- Foodies/resources/views/admin/menus/create.blade.php
+++ Foodies/resources/views/admin/menus/create.blade.php
@@ -15,37 +15,50 @@
 
             <div class="m-2 p-2 bg-white rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('admin.menus.store') }}" enctype="multipart/form-data">
+                        @csrf
                         <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700">Name</label>
+                            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
                             <div class="mt-1">
-                                <input type="text" id="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="text" id="name" name="name"
+                                       class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
                         <div class="sm:col-span-6">
                             <label for="title" class="block text-sm font-medium text-gray-700">Image</label>
                             <div class="mt-1">
-                                <input type="file" id="image" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input type="file" id="image" name="image"
+                                       class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
+                            <div class="mt-1">
+                                <input type="number" min="0.00" max="10000.00" step="0.01" id="price" name="price"
+                                       class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
                         <div class="sm:col-span-6 pt-5">
                             <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
                             <div class="mt-1">
-                                <textarea id="body" rows="3" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                <textarea id="body" rows="3" name="description" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
                             </div>
                         </div>
                         <div class="sm:col-span-6 pt-5">
-                            <label for="body" class="block text-sm font-medium text-gray-700">Categories</label>
+                            <label for="categories" class="block text-sm font-medium text-gray-700">Categories</label>
                             <div class="mt-1">
-                                <select multiple>
+                                <select id="categories" name="categories[]" class="form-multiselect block w-full mt-1"
+                                        multiple>
                                     @foreach($categories as $category)
-                                        <option>{{ $category->name }}</option>
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
                         </div>
                         <div class="mt-6">
-                            <button type="submit" class="px-4 py-2 bg-indigo-50 hover:bg-indigo-200 rounded-lg">Add</button>
+                            <button type="submit" class="px-4 py-2 bg-indigo-50 hover:bg-indigo-200 rounded-lg">
+                                Add
+                            </button>
                         </div>
                     </form>
                 </div>
